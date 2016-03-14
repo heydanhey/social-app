1ed1771085aaa58bf40211d41d95dd7701a0d4a9
heydanhey <danhey@gmail.com>
2016-03-13 23:37:59 -0500

    fixed javascript alerts bug

--- app/assets/javascripts/weefs.js
+++ app/assets/javascripts/weefs.js
@@ -1,4 +1,4 @@
-alert fadeouts
+//alert fadeouts
 $(document).ready (function(){
    $(".alert-dismissible").fadeTo(2000, 500).slideUp(500, function(){
        $(".alert-dismissible").alert('close');
