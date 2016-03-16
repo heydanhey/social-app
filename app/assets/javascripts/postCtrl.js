f8a6e035f555817b9ec1405d6ede110b684edfd5
heydanhey <danhey@gmail.com>
2016-03-16 11:54:48 -0500

    added touch controls

--- app/assets/javascripts/postCtrl.js
+++ app/assets/javascripts/postCtrl.js
@@ -35,6 +35,8 @@
         });
       };
 
+      //This is the js for the keymapping of arrows
+
       $(document).keydown(function(e){
           switch (e.which){
           case 37:    //left arrow key
@@ -84,6 +86,172 @@
               break;
           }
       });
+
+      //Animation function called by the swipe event
+
+      // var animateThis = function(e, direction) {
+      //   var idNumber = gon.post.id;
+      //   var stats = gon.stats;
+      //   var idString = idNumber.toString();
+      //   switch (direction){
+      //   case 'left':    //left arrow key
+      //       $(".box").finish().animate({
+      //           left: "-=1500",
+      //           // backgroundColor: '#ff0004'
+      //       });
+      //       $(".stat-box .value").html(stats[3]);
+      //       document.location = "/posts/" + idString + "?annoyed=id";
+      //       break;
+      //   case 'up':    //up arrow key
+      //       $(".box").finish().animate({
+      //           top: "-=1500",
+      //           // backgroundColor: '#02f90e'
+      //       });
+      //       $(".stat-box .value").html(stats[0]);
+      //       document.location = "/posts/" + idString + "?excited=id";
+      //       break;
+      //   case 'right':    //right arrow key
+      //       $(".box").finish().animate({
+      //           left: "+=1500",
+      //           // backgroundColor: '#fc6635'
+      //       });
+      //       $(".stat-box .value").html(stats[1]);
+      //       document.location = "/posts/" + idString + "?amused=id";
+      //       break;
+      //   case 'down':    //bottom arrow key
+      //       $(".box").finish().animate({
+      //           top: "+=1500",
+      //           // backgroundColor: '#02c8ff'
+      //       });
+      //       $(".stat-box .value").html(stats[2]);
+      //       document.location = "/posts/" + idString + "?sympathetic=id";
+      //       break;
+      //     }
+      // }
+
+      //This is the js for the touch controls
+
+      function swipedetect(el, callback){
+      
+        var touchsurface = el,
+        swipedir,
+        startX,
+        startY,
+        distX,
+        distY,
+        threshold = 150, //required min distance traveled to be considered swipe
+        restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+        allowedTime = 300, // maximum time allowed to travel that distance
+        elapsedTime,
+        startTime,
+        handleswipe = callback || function(swipedir){}
+      
+        touchsurface.addEventListener('touchstart', function(e){
+            var touchobj = e.changedTouches[0]
+            swipedir = 'none'
+            dist = 0
+            startX = touchobj.pageX
+            startY = touchobj.pageY
+            startTime = new Date().getTime() // record time when finger first makes contact with surface
+            e.preventDefault()
+        }, false)
+      
+        touchsurface.addEventListener('touchmove', function(e){
+            e.preventDefault() // prevent scrolling when inside DIV
+        }, false)
+      
+        touchsurface.addEventListener('touchend', function(e){
+            var touchobj = e.changedTouches[0]
+            distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+            distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+            elapsedTime = new Date().getTime() - startTime // get time elapsed
+            if (elapsedTime <= allowedTime){ // first condition for swipe met
+                if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+                    swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+                }
+                else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+                    swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+                }
+            }
+            handleswipe(swipedir)
+            e.preventDefault()
+        }, false)
+    }
+
+    window.addEventListener('load', function(){
+        var idNumber = gon.post.id;
+        var idString = idNumber.toString();
+        var el = document.getElementById('touchsurface2')
+        var inner = document.getElementById('inner')
+        var hidetimer = null
+        swipedetect(el, function(swipedir){
+            if (swipedir != 'none'){
+                clearTimeout(hidetimer)
+                // alert("SWIPE detected!");
+                switch (swipedir) {
+            case "left":
+
+                $(".box").animate({
+                  backgroundColor: '#ff0004',
+                  left: "-=1500"
+                }, 500, function(){$scope.getNextPost("/posts/" + $scope.activeId + "?annoyed=id")});
+                $(".stat-box .value").html($scope.activeStat4 + "%");
+                $(".stat-box").css('color', '#E08484');
+                $(".stat-box .value").append('<img src="/assets/4.png" >');
+
+                //alert(idString);
+                // animateThis(el, 'left');
+                // document.location = "/posts/" + idString + "?annoyed=id";
+                break;
+            case "up":
+
+                $(".box").animate({
+                  backgroundColor: '#02f90e',
+                  top: "-=1500"
+                }, 500, function(){$scope.getNextPost("/posts/" + $scope.activeId + "?excited=id")});
+                $(".stat-box .value").html($scope.activeStat1 + "%");
+                $(".stat-box").css('color', '#70C04B');
+                $(".stat-box .value").append('<img src="/assets/1.png" >');
+
+                // alert('up');
+                // animateThis(el, 'up');
+                // document.location = "/posts/" + idString + "?excited=id";
+                break;
+            case "right":
+
+                $(".box").animate({
+                    backgroundColor: '#fc6635',
+                    left: "+=2000"
+                }, 500, function(){$scope.getNextPost("/posts/" + $scope.activeId + "?amused=id")});
+                $(".stat-box .value").html($scope.activeStat2 + "%");
+                $(".stat-box").css('color', '#F8AF46');
+                $(".stat-box .value").append('<img src="/assets/2.png" >');
+
+                // alert('right');
+                // animateThis(el, 'right');
+                // document.location = "/posts/" + idString + "?amused=id";
+                break;
+            case "down":
+
+                $(".box").animate({
+                  backgroundColor: '#02c8ff',
+                  top: "+=1500"
+                }, 500, function(){$scope.getNextPost("/posts/" + $scope.activeId + "?sympathetic=id")});
+                $(".stat-box .value").html($scope.activeStat3 + "%");
+                $(".stat-box").css('color', '#2EC6DC');
+                $(".stat-box .value").append('<img src="/assets/3.png" >');
+                
+                // alert('down');
+                // animateThis(el, 'down');
+                // document.location = "/posts/" + idString + "?sympathetic=id";
+                break;
+        }
+                // hidetimer = setTimeout(function(){ // reset background image after 1 second
+                //     inner.style.background = ''
+                // }, 1000)
+            }
+        })
+    }, false)
 
 
       
--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -89,45 +89,45 @@ $(document).ready(function(e){
 
 
 
-var animateThis = function(e, direction) {
-    var idNumber = gon.post.id;
-    var stats = gon.stats;
-    var idString = idNumber.toString();
-    switch (direction){
-    case 'left':    //left arrow key
-        $(".box").finish().animate({
-            left: "-=1500",
-            // backgroundColor: '#ff0004'
-        });
-        $(".stat-box .value").html(stats[3]);
-        document.location = "/posts/" + idString + "?annoyed=id";
-        break;
-    case 'up':    //up arrow key
-        $(".box").finish().animate({
-            top: "-=1500",
-            // backgroundColor: '#02f90e'
-        });
-        $(".stat-box .value").html(stats[0]);
-        document.location = "/posts/" + idString + "?excited=id";
-        break;
-    case 'right':    //right arrow key
-        $(".box").finish().animate({
-            left: "+=1500",
-            // backgroundColor: '#fc6635'
-        });
-        $(".stat-box .value").html(stats[1]);
-        document.location = "/posts/" + idString + "?amused=id";
-        break;
-    case 'down':    //bottom arrow key
-        $(".box").finish().animate({
-            top: "+=1500",
-            // backgroundColor: '#02c8ff'
-        });
-        $(".stat-box .value").html(stats[2]);
-        document.location = "/posts/" + idString + "?sympathetic=id";
-        break;
-    }
-}
+// var animateThis = function(e, direction) {
+//     var idNumber = gon.post.id;
+//     var stats = gon.stats;
+//     var idString = idNumber.toString();
+//     switch (direction){
+//     case 'left':    //left arrow key
+//         $(".box").finish().animate({
+//             left: "-=1500",
+//             // backgroundColor: '#ff0004'
+//         });
+//         $(".stat-box .value").html(stats[3]);
+//         document.location = "/posts/" + idString + "?annoyed=id";
+//         break;
+//     case 'up':    //up arrow key
+//         $(".box").finish().animate({
+//             top: "-=1500",
+//             // backgroundColor: '#02f90e'
+//         });
+//         $(".stat-box .value").html(stats[0]);
+//         document.location = "/posts/" + idString + "?excited=id";
+//         break;
+//     case 'right':    //right arrow key
+//         $(".box").finish().animate({
+//             left: "+=1500",
+//             // backgroundColor: '#fc6635'
+//         });
+//         $(".stat-box .value").html(stats[1]);
+//         document.location = "/posts/" + idString + "?amused=id";
+//         break;
+//     case 'down':    //bottom arrow key
+//         $(".box").finish().animate({
+//             top: "+=1500",
+//             // backgroundColor: '#02c8ff'
+//         });
+//         $(".stat-box .value").html(stats[2]);
+//         document.location = "/posts/" + idString + "?sympathetic=id";
+//         break;
+//     }
+// }
 
 
 
@@ -140,91 +140,91 @@ var animateThis = function(e, direction) {
 
 
 
-function swipedetect(el, callback){
+// function swipedetect(el, callback){
   
-    var touchsurface = el,
-    swipedir,
-    startX,
-    startY,
-    distX,
-    distY,
-    threshold = 150, //required min distance traveled to be considered swipe
-    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-    allowedTime = 300, // maximum time allowed to travel that distance
-    elapsedTime,
-    startTime,
-    handleswipe = callback || function(swipedir){}
+//     var touchsurface = el,
+//     swipedir,
+//     startX,
+//     startY,
+//     distX,
+//     distY,
+//     threshold = 150, //required min distance traveled to be considered swipe
+//     restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+//     allowedTime = 300, // maximum time allowed to travel that distance
+//     elapsedTime,
+//     startTime,
+//     handleswipe = callback || function(swipedir){}
   
-    touchsurface.addEventListener('touchstart', function(e){
-        var touchobj = e.changedTouches[0]
-        swipedir = 'none'
-        dist = 0
-        startX = touchobj.pageX
-        startY = touchobj.pageY
-        startTime = new Date().getTime() // record time when finger first makes contact with surface
-        e.preventDefault()
-    }, false)
+//     touchsurface.addEventListener('touchstart', function(e){
+//         var touchobj = e.changedTouches[0]
+//         swipedir = 'none'
+//         dist = 0
+//         startX = touchobj.pageX
+//         startY = touchobj.pageY
+//         startTime = new Date().getTime() // record time when finger first makes contact with surface
+//         e.preventDefault()
+//     }, false)
   
-    touchsurface.addEventListener('touchmove', function(e){
-        e.preventDefault() // prevent scrolling when inside DIV
-    }, false)
+//     touchsurface.addEventListener('touchmove', function(e){
+//         e.preventDefault() // prevent scrolling when inside DIV
+//     }, false)
   
-    touchsurface.addEventListener('touchend', function(e){
-        var touchobj = e.changedTouches[0]
-        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-        elapsedTime = new Date().getTime() - startTime // get time elapsed
-        if (elapsedTime <= allowedTime){ // first condition for swipe met
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
-            }
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
-            }
-        }
-        handleswipe(swipedir)
-        e.preventDefault()
-    }, false)
-}
+//     touchsurface.addEventListener('touchend', function(e){
+//         var touchobj = e.changedTouches[0]
+//         distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+//         distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+//         elapsedTime = new Date().getTime() - startTime // get time elapsed
+//         if (elapsedTime <= allowedTime){ // first condition for swipe met
+//             if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+//                 swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+//             }
+//             else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+//                 swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+//             }
+//         }
+//         handleswipe(swipedir)
+//         e.preventDefault()
+//     }, false)
+// }
 
-window.addEventListener('load', function(){
-    var idNumber = gon.post.id;
-    var idString = idNumber.toString();
-    var el = document.getElementById('touchsurface2')
-    var inner = document.getElementById('inner')
-    var hidetimer = null
-    swipedetect(el, function(swipedir){
-        if (swipedir != 'none'){
-            clearTimeout(hidetimer)
-            // alert("SWIPE detected!");
-            switch (swipedir) {
-        case "left":
-            //alert(idString);
-            animateThis(el, 'left');
-            // document.location = "/posts/" + idString + "?annoyed=id";
-            // break;
-        case "up":
-            // alert('up');
-            animateThis(el, 'up');
-            // document.location = "/posts/" + idString + "?excited=id";
-            // break;
-        case "right":
-            // alert('right');
-            animateThis(el, 'right');
-            // document.location = "/posts/" + idString + "?amused=id";
-            // break;
-        case "down":
-            // alert('down');
-            animateThis(el, 'down');
-            // document.location = "/posts/" + idString + "?sympathetic=id";
-            // break;
-    }
-            // hidetimer = setTimeout(function(){ // reset background image after 1 second
-            //     inner.style.background = ''
-            // }, 1000)
-        }
-    })
-}, false)
+// window.addEventListener('load', function(){
+//     var idNumber = gon.post.id;
+//     var idString = idNumber.toString();
+//     var el = document.getElementById('touchsurface2')
+//     var inner = document.getElementById('inner')
+//     var hidetimer = null
+//     swipedetect(el, function(swipedir){
+//         if (swipedir != 'none'){
+//             clearTimeout(hidetimer)
+//             // alert("SWIPE detected!");
+//             switch (swipedir) {
+//         case "left":
+//             //alert(idString);
+//             animateThis(el, 'left');
+//             // document.location = "/posts/" + idString + "?annoyed=id";
+//             // break;
+//         case "up":
+//             // alert('up');
+//             animateThis(el, 'up');
+//             // document.location = "/posts/" + idString + "?excited=id";
+//             // break;
+//         case "right":
+//             // alert('right');
+//             animateThis(el, 'right');
+//             // document.location = "/posts/" + idString + "?amused=id";
+//             // break;
+//         case "down":
+//             // alert('down');
+//             animateThis(el, 'down');
+//             // document.location = "/posts/" + idString + "?sympathetic=id";
+//             // break;
+//     }
+//             // hidetimer = setTimeout(function(){ // reset background image after 1 second
+//             //     inner.style.background = ''
+//             // }, 1000)
+//         }
+//     })
+// }, false)
 
 
 
