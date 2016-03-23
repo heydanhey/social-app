5e9b529a99e93803aae81258232c19f27eca0782
heydanhey <danhey@gmail.com>
2016-03-22 23:32:00 -0500

    Added unique call for posts archive

--- app/controllers/weefs_controller.rb
+++ app/controllers/weefs_controller.rb
@@ -25,11 +25,19 @@ class WeefsController < ApplicationController
     gon.id = params[:id]
     # @weef = Weef.find(params[:id])
     if params[:their_posts]
+
       @see_post = true
-      @posts = Post.where(user_id: params[:id])
+
+      @posts = Post.where(user_id: params[:id]).select(:id, :user_id).select(:text, :emotion_id).distinct.limit(25)
+
+      # @posts = @posts.select(:id, :user_id).select(:text, :emotion_id).distinct
+
     elsif params[:their_weactions]
+
       @see_post = false
+
       @weactions = Weaction.where(user_id: params[:id]).limit(25)
+
     end
     
     gon.area_chart = [0,0,0,0]
